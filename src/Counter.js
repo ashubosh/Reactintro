1118ecd9b744d2b1875f07b8f635203d4a2dc240
ashubosh <ashubosh@gmail.com>
2023-01-06 03:20:43 +0530

    day3

--- src/Counter.js
+++ src/Counter.js
@@ -7,12 +7,31 @@ constructor(){
         Count:0
     }
 }
+increment(){
+    this.setState({
+        Count:this.state.Count + 1
+    })
+}
+decrement(){
+    this.setState({
+        Count:this.state.Count -1
+    })
+}
+changeValue(value){
+    this.setState({
+        Count:value
+    })
+}
 render(){
     return (
     <div className ="Counter-component">
     <p>Count-{this.state.Count}</p>
-    <button>+ Increment</button>
-    <button>- Decrement</button>
+    <input type="number" 
+    value = {this.state.Count}
+    onChange={(event)=>this.changeValue(event.target.value)}>
+    </input>
+    <button onClick={()=>this.increment()}>+ Increment</button>
+    <button onClick={()=>this.decrement()}>- Decrement</button>
     </div>
     )
 }
